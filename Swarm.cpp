878784d392a63715632a240e015bf709885b7488
CodeOx <mittalchetan98@gmail.com>
2018-05-02 19:06:30 +0530

    Update Swarm.cpp

--- Swarm.cpp
+++ Swarm.cpp
@@ -1,15 +1,34 @@
 #include <iostream>
-#include <pthread.h>
 #include <cmath>
+#include <QVector3D>
+#include <QMatrix4x4>
 #include "Swarm.h"
 
 #define INITIAL_SIZE 4
 
-struct thread_data {
-    float time;
-    Boid b;
-    Swarm s;
-};
+float magnitude1(Vector v){
+    float x = v.getFirst();
+    float y = v.getSecond();
+    float z = v.getThird();
+
+    float mag = std::pow((x*x)+(y*y)+(z*z),0.5);
+
+    return mag;
+}
+
+float dotProduct1(Vector v1, Vector v2){
+    return (v1.getFirst() * v2.getFirst()) + (v1.getSecond() * v2.getSecond()) + (v1.getThird() * v2.getThird());
+}
+
+int getRotationAngle1(Vector v1, Vector v2){
+    if (magnitude1(v1) != 0 && magnitude1(v2)!= 0){
+        std::cout<<"1 "<<magnitude1(v1)<< " "<<magnitude1(v2)<<std::endl;
+        float a = (((dotProduct1(v1,v2))/((magnitude1(v1)*magnitude1(v2)))));
+        std::cout<<"3 "<<a<<std::endl;
+        return (int)57.296 * (std::acos(a));
+    }
+    return 0;
+}
 
 Swarm::Swarm(){
 	for(int i = 0; i < INITIAL_SIZE; i++){
@@ -67,7 +86,7 @@ Vector Swarm::forceSeparation(Boid b){
     float proportionalityConstant = -1.0;
     float exponent = -1.0;
     float constant = 0.0;
-    float minDistance = 100.0;
+    float minDistance = 10.0;
     float maxForce = 50.0;
     float force = 0.0;
 
@@ -122,7 +141,7 @@ Vector Swarm::forceAlignment(Boid b){
     return force; 
 }
 
-Vector Swarm::forceCenter(Boid b){
+void Swarm::applyForceCenter(Boid &b){
     float x = 0.0;
     float y = 0.0;
     float z = 0.0;
@@ -132,16 +151,35 @@ Vector Swarm::forceCenter(Boid b){
     y = location_b.getSecond();
     z = location_b.getThird();
 
-    float proportionalityConstant = 1.0;
-    float exponent = 1.0;
+    Vector velocity_b = b.getVelocity();
 
-    float force_x = proportionalityConstant*(std::pow((-1*x),exponent));
-    float force_y = proportionalityConstant*(std::pow((-1*y),exponent));
-    float force_z = proportionalityConstant*(std::pow((-1*z),exponent));
+    QVector3D position(x,y,z);
+    QVector3D velocity(velocity_b.getFirst(),velocity_b.getSecond(),velocity_b.getThird());
+    QVector3D normal = QVector3D::crossProduct(position,velocity);
 
-    Vector force(force_x, force_y, force_z);
+    std::cout<<"vel here    x: "<<velocity_b.getFirst()<<" y: "<<velocity_b.getSecond()<<" z: "<<velocity_b.getThird()<<std::endl;
+    std::cout<<"loc here    x: "<<location_b.getFirst()<<" y: "<<location_b.getSecond()<<" z: "<<location_b.getThird()<<std::endl;
 
-    return force;
+    QMatrix4x4 m1;
+    if(!(normal.x() == 0 && normal.y() == 0 && normal.z() == 0))
+    {
+        m1.rotate(-90, normal);
+        position = position * m1;
+        location_b.setFirst(position.x());
+        location_b.setSecond(position.y());
+        location_b.setThird(position.z());
+    }
+
+    int angle = getRotationAngle1(location_b,velocity_b);
+    if (angle > 90){
+        angle -= 90;
+    }
+    float ratio = -0.2;
+
+    std::cout<<"angle   :   "<<angle<<std::endl;
+
+    if(x*x + y*y + z*z > 40.0)
+        b.applyRotation(angle*ratio, normal);
 }
 
 Vector Swarm::forceDrag(Boid b){
@@ -172,59 +210,61 @@ Vector Swarm::forceDrag(Boid b){
     return force;
 }
 
-void* updateBoid(void* threadarg){
-    struct thread_data *my_data;
-    my_data = (struct thread_data *) threadarg;
-    float time;
-    Boid b;
-    Swarm s;
-    time = my_data->time;
-    b = my_data->b;
-    s = my_data->s;
-    b.update(time);
-    Vector force1 = s.forceCohesion(b);
-    Vector force2 = s.forceCenter(b);
-    Vector force3 = s.forceDrag(b);
-    Vector force4 = s.forceSeparation(b);
-    Vector force5 = s.forceAlignment(b);
-    Vector force;
-    force.setFirst(force1.getFirst() + force2.getFirst() + force3.getFirst() + force4.getFirst() + force5.getFirst());
-    force.setSecond(force1.getSecond() + force2.getSecond() + force3.getSecond() + force4.getSecond() + force5.getSecond());
-    force.setThird(force1.getThird() + force2.getThird() + force3.getThird() + force4.getThird() + force5.getThird());
-    b.applyForce(force);
+Vector Swarm::forceCenter(Boid b){
+    float x = 0.0;
+    float y = 0.0;
+    float z = 0.0;
 
-    std::cout<< "cohesion   "<<force1.getFirst()<<" "<<force1.getSecond()<<" "<<force1.getThird()<<std::endl;
-    std::cout<< "center   "<<force2.getFirst()<<" "<<force2.getSecond()<<" "<<force2.getThird()<<std::endl;
-    std::cout<< "drag   "<<force3.getFirst()<<" "<<force3.getSecond()<<" "<<force3.getThird()<<std::endl;
-    std::cout<< "seperation   "<<force4.getFirst()<<" "<<force4.getSecond()<<" "<<force4.getThird()<<std::endl;
-    std::cout<< "align   "<<force5.getFirst()<<" "<<force5.getSecond()<<" "<<force5.getThird()<<std::endl;
+    Vector location_b = b.getLocation();
+    x = location_b.getFirst();
+    y = location_b.getSecond();
+    z = location_b.getThird();
 
-}
+    float proportionalityConstant = 0.0;
 
-void Swarm::update(float t){
-    int NUM_THREADS = swarm.size();
-
-    pthread_t threads[NUM_THREADS];
-    struct thread_data td[NUM_THREADS];
-    int rc;
-    Swarm sw;
-    
-	for (int i = 0; i < NUM_THREADS; i++){
-        td[i].time = t;
-        td[i].b = swarm[i];
-        td[i].s = sw;
-        rc = pthread_create(&threads[i], NULL, updateBoid, (void *)&td[i]);
-        if (rc) {
-          std::cout << "Error:unable to create thread," << rc << std::endl;
-          exit(-1);
-        }
-	}
-    //Wait for all threads to finish
-    for (int i = 0; i < NUM_THREADS; i++){
-        pthread_join(threads[i], NULL);
+    Vector velocity_b = b.getVelocity();
+    float mag_v = (velocity_b.getFirst()*velocity_b.getFirst()) + (velocity_b.getSecond()*velocity_b.getSecond()) + (velocity_b.getThird()*velocity_b.getThird());
+    if (mag_v <1000 && mag_v > 0){
+        proportionalityConstant = 1000/mag_v;
     }
+
+    float exponent = 1.0;
+
+    float force_x = proportionalityConstant*(-x/magnitude1(location_b));
+    float force_y = proportionalityConstant*(-y/magnitude1(location_b));
+    float force_z = proportionalityConstant*(-z/magnitude1(location_b));
+
+    Vector force(force_x, force_y, force_z);
+
+    return force;
 }
 
+void Swarm::update(float time){
+	for (int i = 0; i < swarm.size(); i++){
+		swarm[i].update(time);
+        Vector force1 = forceCohesion(swarm[i]);
+        Vector force2 = forceCenter(swarm[i]);
+        Vector force3 = forceDrag(swarm[i]);
+        Vector force4 = forceSeparation(swarm[i]);
+        Vector force5 = forceAlignment(swarm[i]);
+        //Vector force2(0.0,0.0,0.0);
+        //Vector force3(0.0,0.0,0.0);
+        Vector force;
+        force.setFirst(force1.getFirst() + force2.getFirst() + force3.getFirst() + force4.getFirst() + force5.getFirst());
+        force.setSecond(force1.getSecond() + force2.getSecond() + force3.getSecond() + force4.getSecond() + force5.getSecond());
+        force.setThird(force1.getThird() + force2.getThird() + force3.getThird() + force4.getThird() + force5.getThird());
+        applyForceCenter(swarm[i]);
+        swarm[i].applyForce(force);
+        if(i == 1){
+            std::cout<< "cohesion   "<<force1.getFirst()<<" "<<force1.getSecond()<<" "<<force1.getThird()<<std::endl;
+            std::cout<< "center   "<<force2.getFirst()<<" "<<force2.getSecond()<<" "<<force2.getThird()<<std::endl;
+            std::cout<< "drag   "<<force3.getFirst()<<" "<<force3.getSecond()<<" "<<force3.getThird()<<std::endl;
+            std::cout<< "seperation   "<<force4.getFirst()<<" "<<force4.getSecond()<<" "<<force4.getThird()<<std::endl;
+            std::cout<< "align   "<<force5.getFirst()<<" "<<force5.getSecond()<<" "<<force5.getThird()<<std::endl;
+        }
+        //swarm[i].update
+	}
+}
 
 Vector Swarm::getCOM(){
 	float x = 0.0;
